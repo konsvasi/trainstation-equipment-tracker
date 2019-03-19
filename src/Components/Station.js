e65def211b3f677c4b3cdc7e87d3f0647a9ff315
Kostas V <konsvass@outlook.com>
2019-03-19 12:02:09 +0100

    implemented the display of equipment in the Station component and also added the api call to get the details. Added some simple styles to equipment component

--- src/Components/Station.js
+++ src/Components/Station.js
@@ -1,4 +1,5 @@
 import React, {Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../Styles/Station.css';
 
 class Station extends Component {
@@ -11,17 +12,47 @@ class Station extends Component {
     }
   }
 
+  //Makes an api call with the id of the clicked station
+  // to get the results about the equipment
+  getStationDetails = async (stationId) => {
+    const response = await fetch(`https://api.deutschebahn.com/fasta/v2/facilities?stationnumber=${stationId}`, {
+      headers: new Headers({
+        Authorization: 'Bearer 241ceeefb40850211177e76d041f442b' 
+      })
+    });
+
+    const results = await response.json();
+    return results;
+  }
+
   componentDidMount() {
     this.setState({
       name: this.props.location.state.name
     })
+    const stationId = this.props.match.params.id;
+    this.getStationDetails(stationId).then(data => {
+      this.setState({
+        equipment: data
+      })
+    })
   }
 
   render() {
+    const equipment = this.state.equipment.map(eq => (
+      <div key={eq.equipmentnumber.toString()}>
+        <h2>{eq.type}</h2>
+        <p>{eq.description}</p>
+        <p>{eq.state}</p>
+      </div>
+    ))
+
     return (
       <div className="station">
         <h1>{this.state.name}</h1>
-        <p>Equipment</p>
+        <div className="equipment-container">
+          {equipment}
+          <Link to="/">back</Link>
+        </div>
       </div>
     )
   }
