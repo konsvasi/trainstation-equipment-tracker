f10b4c4aa877aaf14ab1264ce82f55028da5bf56
Kostas V <konsvass@outlook.com>
2019-03-19 10:54:31 +0100

    added AwesomeDebounce package and implemented event handler for searching and getting results from the API

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import '../Styles/Searchbar.css';
+
+// Api where the search call will happen
+const searchApi = searchTerm => (
+  fetch(`https://api.deutschebahn.com/stada/v2/stations?searchstring=*${searchTerm}*`, {
+        headers: new Headers({
+          Authorization: 'Bearer 241ceeefb40850211177e76d041f442b' 
+        })
+      }
+    )
+)
+
+// The debounced version of the searchAPI call
+// this is being used to reduce the number of requests
+// whenever an onchange event is triggered on the input component
+const searchAPIDebounced = AwesomeDebouncePromise(searchApi, 500);
 
 class Searchbar extends Component {
   constructor(props) {
@@ -9,15 +25,21 @@ class Searchbar extends Component {
     }
   }
 
-  handleChange = (ev) => {
-    
+  handleChange = async (ev) => {
+    // clear result list
+    this.setState({result: []});
+    const result = await searchAPIDebounced(ev.target.value);
+    const data = await result.json();
+    this.setState({
+      result: data.result
+    })
   }
 
   render() {
     let listOfResults;
     if(this.state.result.length > 0) {
       listOfResults = this.state.result.map(station => {
-        return <li key={station.name}>{station.name}</li>
+        return <li key={station.number}>{station.name}</li>
       })
     }
 
